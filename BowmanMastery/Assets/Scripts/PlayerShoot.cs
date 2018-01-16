fef28dbcd69db2db9edd936699efc6a064ccd13c
Thomas Cianfarani <thomas.ci83@gmail.com>
2018-01-16 15:13:39 +0100

    Implémentation de la visée.
    Regroupement des GO des bras dans un empty "Bras".
    Script de Chaos réutilisé pour la rotation.
    Création d'un script Arrow vide en prévision de la suite.

---
+++ BowmanMastery/Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour {
+
+    public GameObject weaponMesh;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        OrientationArme();
+    }
+
+    /// <summary>
+    /// Rotation de l'arme vers le curseur de la souris.
+    /// </summary>
+    private void OrientationArme()
+    {
+        Vector3 diff = Camera.main.ScreenToWorldPoint(Input.mousePosition) - weaponMesh.transform.position;
+        diff.Normalize();
+
+        float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+
+        weaponMesh.transform.rotation = Quaternion.Euler(0f, 0f, rot_z - 94f);
+    }
+}
